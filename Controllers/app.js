dc3669db74cd43699087facd18b432fc0cbe1c51
Northy <adarsharyanmuz@gmail.com>
2022-01-30 10:58:25 +0530

    not-found res

--- Controllers/app.js
+++ Controllers/app.js
@@ -1,19 +1,68 @@
+const Task=require('../Models/Task')
 
-const getAllitems=(req,res)=>{
-    res.send('All items');
+const getAllitems= async (req,res)=>{
+    try {
+        const allTasks=await Task.find({})
+        res.status(200).json({tasks:allTasks});
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    
 }
 
-const additem=(req,res)=>{
-    res.json(req.body)
+const additem= async(req,res)=>{
+    try {
+        const task= await Task.create(req.body) 
+        res.status(201).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+   
 }
-const updateitem=(req,res)=>{
-    res.send('update item')
+
+const getitem= async(req,res)=>{
+    try {
+        const {id:taskID}=req.params;
+        const task=await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`No task with id:${taskID}`})
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    
 }
-const getitem=(req,res)=>{
-    res.send('get single item')
+const deleteitem= async(req,res)=>{
+    try {
+        const {id:taskID}=req.params;
+        const task=await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id:${taskID}`})
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+   
+
+    
 }
-const deleteitem=(req,res)=>{
-    res.send('delete item')
+const updateitem=async(req,res)=>{
+    try {
+        const {id:taskID}=req.params;
+        
+        const task=await Task.findByIdAndUpdate({_id:taskID},req.body,{
+            new:true,
+            runValidators:true,
+        })
+        if(!task){
+            return res.status(404).json({msg:`No task with id:${taskID}`})
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 module.exports={
--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-const connectDb=require('./db/Connect');
+
 
 const express=require('express');
 
 const app=express();
 const router =require('./Routes/app')
 const connectDB=require('./db/Connect');
+const notFound=require('./middleware/not-found')
 require('dotenv').config()
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json())
+
 //routes
 app.get('/',(req,res)=>{
     res.send('Server is running');
 
 })
 
-app.use('/api/v/Routes',router)
+app.use('/api/v1/tasks',router)
+app.use(notFound);
+
 
 const port=3000;
 
